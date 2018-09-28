ed624e9af1cc09b09bb28ef4ae0d8f3f2a0453ad
Yorick.yuan <wowyorick@126.com>
2018-09-28 16:43:27 +0800

    first

---
+++ chapter1/code1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var (
+		a    [11]int
+		i, j int
+	)
+	num := os.Args
+	if len(num) <= 1 || len(num) >= 7 {
+		log.Fatal("请输入五个数字")
+	}
+	num = num[1:6]
+	log.Println(num)
+	for i = 0; i < len(num); i++ {
+		fmt.Println("arg:", num[i])
+		ind, _ := strconv.ParseInt(num[i], 10, 64)
+		a[ind]++
+		fmt.Println("a[ind]:", a[ind])
+	}
+
+	log.Println(a)
+	for i = 0; i < len(a); i++ {
+		for j = 1; j <= a[i]; j++ {
+			fmt.Printf("%d", i)
+		}
+	}
+}
