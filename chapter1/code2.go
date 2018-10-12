9204cf9e2e900d64a6359ed5ba276176436bc81a
Yorick.yuan <wowyorick@126.com>
2018-10-12 17:49:40 +0800

    code2

--- chapter1/code2.go
+++ chapter1/code2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,10 +17,26 @@ func main() {
 		return
 	}
 	fmt.Printf("your num is %s", input)
-	nums := strings.Split(input, " ")
-	fmt.Printf("num len %d ", len(nums))
-	numArr := make([]int, 5)
-	for v := range nums {
-		numArr = append(numArr, v)
+	input = strings.Replace(input, "\n", "", -1)
+	nums := strings.Split(input, "|")
+	fmt.Printf("num len %d, %v ", len(nums), nums)
+	var numArr []int
+	for k, v := range nums {
+		iv, _ := strconv.Atoi(v)
+		numArr = append(numArr, iv)
+		fmt.Println(k, iv)
 	}
+
+	for i := 0; i <= len(numArr)-1; i++ {
+		for j := 0; j < len(numArr)-1; j++ {
+			if numArr[j] < numArr[j+1] {
+				numArr[j], numArr[j+1] = numArr[j+1], numArr[j]
+			}
+		}
+	}
+
+	// for i := 1; i <= len(numArr); i++ {
+	// 	fmt.Printf("%d ", numArr[i])
+	// }
+	fmt.Println(numArr)
 }
